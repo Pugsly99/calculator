5e387c4c12c1e9bf588ae3c298361de76e6a5393
Pugsly99 <rylandadams@yahoo.com>
2020-10-19 13:31:31 -0700

    add muli and sub and div
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,22 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function sub(number1, number2){
+  return number1 - number2;
+}
+
+function muli(number1, number2) {
+  return number1 * number2;
+}
+
+function div(number1, number2){
+  return number1 / number2;
+}
+
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const result = div(number1, number2);
+alert(result);
 
-alert(add(number1, number2));
+
